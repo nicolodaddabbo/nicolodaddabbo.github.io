f5168fa9d39f58f92d65f07ce950a6ca0017cec9
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-10-25 20:43:43 +0200

    feat: added social links to contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,38 @@
 import "./contact.scss"
 import React from 'react'
 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import CallIcon from '@material-ui/icons/Call';
+
 export default function Contact() {
   return (
     <div className='contact' id='contact'>
       <div className="contactHeader">
-        <p className="contactHeaderLeft">Want to work<br/>togheter?</p>
-        <p className="contactHeaderRight">Send me a<br/>message</p>
+        <p className="contactHeaderLeft">Want to work<br />togheter?</p>
+        <p className="contactHeaderRight">Send me a<br />message</p>
       </div>
       <div className="contactEmail">
-        <a className="bigEmail" href="mailto:nicolodaddabbo@gmail.com">nicolodaddabbo<span className="at">@</span>gmail<span className="dot">.</span>com</a>
+        <a className="bigEmail" href="mailto:nicolodaddabbo@gmail.com">nicolo<span className="at"></span>daddabbo<span className="at">@</span>gmail<span className="dot">.</span>com</a>
+      </div>
+      <div className="social">
+        <a href="https://www.linkedin.com/in/nicolodaddabbo/">
+          <LinkedInIcon style={{ fontSize: "50px" }} />
+        </a>
+        <a href="https://instagram.com/nicolodaddabbo">
+          <InstagramIcon style={{ fontSize: "50px" }} />
+        </a>
+        <a href="mailto:nicolodaddabbo@gmail.com">
+          <MailOutlineIcon style={{ fontSize: "50px" }} />
+        </a>
+        <a href="tel:+393273936148">
+          <CallIcon style={{ fontSize: "50px" }} />
+        </a>
+        <a href="https://github.com/nicolodaddabbo">
+          <GitHubIcon style={{ fontSize: "50px" }} />
+        </a>
       </div>
       <div className="footerContainer">
         <div className="footerCredits">
