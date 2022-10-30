6847844949adf74fc32589c1febe0729090dbfb6
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-10-30 18:41:40 +0100

    Updates

---
+++ precache-manifest.4c601e0fd256318839fe8f67339338c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "912f8c3c779cbcf595f9",
+    "url": "/static/css/main.2d5880aa.chunk.css"
+  },
+  {
+    "revision": "912f8c3c779cbcf595f9",
+    "url": "/static/js/main.912f8c3c.chunk.js"
+  },
+  {
+    "revision": "bd83159fbf2b3fba3066",
+    "url": "/static/js/1.bd83159f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92828253a7e5296c2e1eef799050fc65",
+    "url": "/index.html"
+  }
+];
