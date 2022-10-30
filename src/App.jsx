649d692d58d8a30755512871c018afdfd7ebb5d2
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2022-10-30 18:40:19 +0100

    feat: added meta tags for SEO optimization

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import "./app.scss";
 import Home from "./components/home/Home"
 import React from 'react'
 import { BrowserRouter } from "react-router-dom";
+import Helmet from "react-helmet";
 
 export default function App() {
   return(
     <BrowserRouter>
+      <Helmet>
+        <title>Nicolò D'Addabbo Portfolio</title>
+        <meta
+          name="description"
+          content="Freelance Web Developer Portfolio based in Rimini - Italia"
+        />
+        <meta
+          name="keywords"
+          content="Sito Web, Rimini, Web Developer, Freelance, Developer, Full Stack, Portfolio"
+        />
+        <meta 
+          http-equiv="Content-Language" 
+          content="en, it" 
+        />
+      </Helmet>
       <Home/>   
     </BrowserRouter>
   )
