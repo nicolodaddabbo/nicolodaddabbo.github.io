47416050e99d15fab576f3dd941daa5b9f244df0
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2025-05-11 16:21:52 +0200

    fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +7,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Work Sans",
+      cssVariable: "--font-work-sans",
+      weights: ["100 900"]
+    }]
   }
 });
