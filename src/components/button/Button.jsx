f0dff32ce8926635de1d4a58b82eb64742bd9145
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2025-05-13 19:40:23 +0200

    redirect to nicolod.com

--- src/components/button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import "./button.scss"
-import React from 'react'
-
-export default function Button({text, onClick}) {
-  return (
-    <button className="defaultButton" onClick={onClick}>{text}</button>
-  )
-}
